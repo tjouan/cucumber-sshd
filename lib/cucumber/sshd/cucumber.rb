0a02c402f034598c762ee2f9d99563cfd8516850
Thibault Jouan <tj@a13.fr>
2014-04-01 02:38:29 +0000

    Imploment `fast' option

--- lib/cucumber/sshd/cucumber.rb
+++ lib/cucumber/sshd/cucumber.rb
@@ -1,15 +1,23 @@
 require 'cucumber/sshd/server'
 
 Before('@sshd') do
-  @_sshd = Cucumber::SSHD::Server.new(current_dir,
-    wait_ready: @_sshd_wait_ready
-  )
-  @_sshd.make_env
-  @_sshd.start
+  make_server = proc do
+    server = Cucumber::SSHD::Server.new(current_dir, wait_ready: @_sshd_wait_ready)
+    server.make_env
+    server.start
+    server
+  end
+
+  if @_sshd_fast && !@_sshd
+    @_sshd = make_server.call
+    at_exit { @_sshd.stop }
+  else
+    @_sshd = make_server.call
+  end
 
   ENV['HOME'] = File.expand_path current_dir
 end
 
 After('@sshd') do
-  @_sshd.stop
+  @_sshd.stop unless @_sshd_fast
 end
