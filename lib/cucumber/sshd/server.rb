d590d7731859674123a517b2ae0b5b2e07aa390a
Thibault Jouan <tj@a13.fr>
2017-10-19 23:19:19 +0000

    Fix Cucumber::SSHD::Server public interface

--- lib/cucumber/sshd/server.rb
+++ lib/cucumber/sshd/server.rb
@@ -51,16 +51,14 @@ qnLMVQddVitzQP7LEhXbNUuUAzEMfA6rAA==
         @wait_ready = wait_ready
       end
 
-      def command
-        "#{COMMAND} -f #{SSHD_CONFIG_PATH} #{COMMAND_ARGS}"
-      end
-
-      def wait_ready?
-        !!@wait_ready
+      def make_env
+        %w[etc .ssh].map { |e| create_dir_secure e }
+        make_env_server
+        make_env_client
       end
 
       def start
-        cd '.' do
+        cd ?. do
           @pid = fork do
             $stderr.reopen '/dev/null'
             exec command
@@ -70,52 +68,15 @@ qnLMVQddVitzQP7LEhXbNUuUAzEMfA6rAA==
         wait_ready! if wait_ready?
       end
 
-      def wait_ready!
-        TCPSocket.new HOSTNAME, port
-      rescue Errno::ECONNREFUSED
-        sleep 0.05
-        retry
-      end
-
       def stop
         Process.kill('TERM', pid)
         Process.wait(pid)
       end
 
-      def make_env
-        %w[etc .ssh].map { |e| create_dir_secure e }
-
-        write_file_secure KEY_PATH, KEY
-
-        write_file_secure KEY_PUB_PATH, KEY_PUB
-
-        write_file_secure SSHD_CONFIG_PATH, <<-eoh
-Port #{port}
-ListenAddress ::1
-
-Protocol 2
-HostKey #{File.expand_path base_path}/#{KEY_PATH}
-PidFile /dev/null
-UsePrivilegeSeparation no
-Subsystem sftp #{sftp_server_path}
-ForceCommand HOME=#{File.expand_path base_path} sh -c "cd ~; [ -f .ssh/rc ] && . .ssh/rc; $SSH_ORIGINAL_COMMAND"
-        eoh
-
-        write_file_secure SSH_CONFIG_PATH, <<-eoh
-Host                    #{host}
-  HostName              #{HOSTNAME}
-  Port                  #{port}
-  UserKnownHostsFile    #{File.expand_path base_path}/#{SSH_KNOWN_HOSTS_PATH}
-        eoh
-
-        write_file_secure SSH_KNOWN_HOSTS_PATH, "[#{HOSTNAME}]:2222 #{KEY_PUB}"
-      end
-
     private
 
-      def write_file_secure path, content
-        write_file path, content
-        chmod 0600, path
+      def command
+        "#{COMMAND} -f #{SSHD_CONFIG_PATH} #{COMMAND_ARGS}"
       end
 
       def create_dir_secure path
@@ -123,8 +84,49 @@ Host                    #{host}
         chmod 0700, path
       end
 
+      def make_env_client
+        write_file_secure SSH_CONFIG_PATH, <<-eoh
+Host                    #{host}
+  HostName              #{HOSTNAME}
+  Port                  #{port}
+  UserKnownHostsFile    #{File.expand_path base_path}/#{SSH_KNOWN_HOSTS_PATH}
+        eoh
+        write_file_secure SSH_KNOWN_HOSTS_PATH, "[#{HOSTNAME}]:2222 #{KEY_PUB}"
+      end
+
+      def make_env_server
+        write_file_secure KEY_PATH, KEY
+        write_file_secure KEY_PUB_PATH, KEY_PUB
+        write_file_secure SSHD_CONFIG_PATH, <<-eoh
+Port #{port}
+ListenAddress ::1
+Protocol 2
+HostKey #{File.expand_path base_path}/#{KEY_PATH}
+PidFile /dev/null
+UsePrivilegeSeparation no
+Subsystem sftp #{sftp_server_path}
+ForceCommand HOME=#{File.expand_path base_path} sh -c "cd ~; [ -f .ssh/rc ] && . .ssh/rc; $SSH_ORIGINAL_COMMAND"
+        eoh
+      end
+
       def sftp_server_path
         SFTP_SERVER_PATHS.detect { |e| File.exist? e } or SFTP_SERVER_PATHS.first
+      end
+
+      def wait_ready!
+        TCPSocket.new HOSTNAME, port
+      rescue Errno::ECONNREFUSED
+        sleep 0.05
+        retry
+      end
+
+      def wait_ready?
+        !!@wait_ready
+      end
+
+      def write_file_secure path, content
+        write_file path, content
+        chmod 0600, path
       end
     end
   end
